c48b5c46a70a48af6e94ad06cc91f2679b43477a
Mariia20222022 <mariaorlova1818@gmail.com>
2023-03-27 21:00:30 +0300

    changes

--- src/JS/climate/climate.js
+++ src/JS/climate/climate.js
@@ -1,133 +1,36 @@
-let apiKey = "3a829f770a96e34d984658f12d3d8364";
+import { renderWeather } from "./renderWeatherCard";
 
-async function getDefaultWeatherData() {
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&units=metric&appid=${apiKey}`;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=Boston&units=metric&appid=${apiKey}`;
+const apiKey = "3a829f770a96e34d984658f12d3d8364";
+
+async function getAndRenderNewYorkWeather() {
   try {
-    const response = await fetch(url);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metric`
+    );
     const data = await response.json();
-
-    return data;
+    renderWeather(data);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
 
-function updateUI(data) {
-  let temperature = document.getElementById("temperature");
-  let description = document.getElementById("description");
-
-  let location = document.getElementById("location");
-  let img = document.querySelector(".weather__img");
-  console.log(img);
-  let day = document.getElementById(`day`);
-  let temp = Math.round(data.main.temp);
-  temperature.innerHTML = temp + "°";
-  // data.name + " (" + lat + "°, " + long + "°)";
-  description.innerHTML = data.weather[0].main;
-  img.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`
-  );
-
-  location.innerHTML = data.name;
-
-  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-
-  const now = new Date();
-
-  day.innerHTML = days[now.getDay()];
-  date.innerHTML = now.getDate();
-  month.innerHTML = months[now.getMonth()];
-  year.innerHTML = now.getFullYear();
+async function fetchWeatherData(lat, lon) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data;
 }
-getDefaultWeatherData(location)
-  .then(weatherData => {
-    updateUI(weatherData);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+function getWeatherByLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(async position => {
+      const { latitude, longitude } = position.coords;
+      const data = await fetchWeatherData(latitude, longitude);
+      renderWeather(data);
+    });
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
 
-getDefaultWeatherData();
-
-// async function fetchWeatherByGeo(lat, lon) {
-//   const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
-//   try {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// async function getWeatherByLocation() {
-//   if (navigator.geolocation) {
-//     await navigator.geolocation.getCurrentPosition((position) => {
-//       long = position.coords.longitude;
-//       lat = position.coords.latitude;
-//       let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
-//       fetch(url).then((response) => {
-//         response.json().then((data) => {
-//           let temperature = document.getElementById("temperature");
-//           let description = document.getElementById("description");
-
-//           let location = document.getElementById("location");
-//           let img = document.querySelector(".weather__img");
-//           let temp = Math.round(data.main.temp);
-//           temperature.innerHTML = temp + "°";
-//           data.name + " (" + lat + "°, " + long + "°)";
-//           description.innerHTML = data.weather[0].main;
-//           img.setAttribute(
-//             "src",
-//             `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`
-//           );
-
-//           location.innerHTML = data.name;
-
-//           const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-//           const months = [
-//             "Jan",
-//             "Feb",
-//             "Mar",
-//             "Apr",
-//             "May",
-//             "Jun",
-//             "Jul",
-//             "Aug",
-//             "Sep",
-//             "Oct",
-//             "Nov",
-//             "Dec",
-//           ];
-
-//           const now = new Date();
-
-//           day.innerHTML = days[now.getDay()];
-//           date.innerHTML = now.getDate();
-//           month.innerHTML = months[now.getMonth()];
-//           year.innerHTML = now.getFullYear();
-//         });
-//       });
-//     });
-//   } else {
-//     alert(`Please enable location to get weather`);
-//   }
-// }
-
+getAndRenderNewYorkWeather();
 // getWeatherByLocation();
